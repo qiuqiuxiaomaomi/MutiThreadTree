39b5be8d27776c023bedc50102717be5c1fbaaec
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-10-04 12:57:51 +0800

    多线程编程

---
+++ src/main/java/com/bonaparte/service/CommunicatCyclicBarrierService.java
@@ -0,0 +1,10 @@
+package com.bonaparte.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yangmingquan on 2018/10/4.
+ */
+@Service
+public class CommunicatCyclicBarrierService {
+}
